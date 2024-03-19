37fb01cbfa4fb91550e43ef8a00ed9e3fb66f429
Anish Baniya <61052810+anishbania@users.noreply.github.com>
2024-03-19 11:16:48 +0545

    Add files via upload

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+
+        </div>
+        <div className="descriptionbox-description">
+            <p>Ecommerce, or electronic commerce, is the buying and selling of goods and services online. It has revolutionized shopping, allowing consumers to browse vast selections, compare prices from anywhere, and receive deliveries conveniently. Businesses can reach wider audiences, offer competitive prices, and benefit from lower overhead costs. While concerns like security and lack of physical interaction exist, ecommerce continues to grow, driven by mobile shopping, social media integration, and personalization. With new technologies like virtual reality emerging, the future of ecommerce promises even more immersive and seamless shopping experiences.</p>
+            <p>Beyond the convenience and vast selection offered to consumers, ecommerce empowers businesses in several ways. It allows them to establish an online presence, often at a lower cost compared to traditional brick-and-mortar stores. This opens doors to new markets and customer segments, breaking geographical barriers. Additionally, ecommerce platforms provide valuable data on customer behavior and preferences, enabling businesses to tailor their offerings and marketing strategies for greater effectiveness. This data-driven approach fosters customer engagement and loyalty, contributing to long-term business success.</p>
+
+            </div>
+    </div>
+  )
+}
