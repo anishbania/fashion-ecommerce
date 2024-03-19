a453c0b3761a832e7bd00e10f40d19fc762e6470
Anish Baniya <61052810+anishbania@users.noreply.github.com>
2024-03-19 11:21:08 +0545

    Add files via upload

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg';
+import navProfile from '../../assets/nav-profile.svg';
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={navlogo} className='nav-logo' alt="" />
+        <img src={navProfile} alt="" className='nav-profile'/>
+    </div>
+  )
+}
+export default Navbar
